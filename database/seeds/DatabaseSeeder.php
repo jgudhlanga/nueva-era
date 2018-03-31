4fb02ecc1bf52f5abada26b23a49fa0b403f630f
jgudhlanga <james@propay.co.za>
2018-03-31 20:18:30 +0200

    exclude default images from gitignore

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,12 +17,12 @@ class DatabaseSeeder extends Seeder
 	    $this->call(TitlesSeeder::class);
 	    $this->call(GenderSeeder::class);
 	    $this->call(OccupationsSeeder::class);
-	    $this->call(CountriesSeeder::class);
 	    $this->call(MemberTypesTableSeeder::class);
 	    $this->call(AddressTypeTableSeeder::class);
 	    $this->call(RolesTableSeeder::class);
 	    $this->call(UserRolesSeeder::class);
-	    //$this->call(LaratrustSeeder::class);
+        $this->call(CountriesSeeder::class);
+        //$this->call(LaratrustSeeder::class);
 	    /*factory(App\Models\Users\User::class, 1)->create();
 	    factory(App\Models\Users\User::class, 1)->create();
 	    factory(App\Models\Products\Product::class, 50)->create();
